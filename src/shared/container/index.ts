dbbe4903136b7e2407b5f3a8efd4a249c64e36bc
nathan ks <nathanks0210@gmail.com>
2024-08-08 21:30:00 -0300

    refactor: Remove unused Papel and Termos-Privacidade code and files

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,10 +2,6 @@ import { CidadeRepository } from '@modules/Cidade/repository/CidadeRepository';
 import { ICidadeRepository } from '@modules/Cidade/repository/ICidadeRepository.interface';
 import { EstadoRepository } from '@modules/Estado/repository/EstadoRepository';
 import { IEstadoRepository } from '@modules/Estado/repository/IEstadoRepository.interface';
-import { IPapelRepository } from '@modules/Papel/repository/IPapelRepository.interface';
-import { PapelRepository } from '@modules/Papel/repository/PapelRepository';
-import { ITermoPrivacidadeRepository } from '@modules/Termos-Privacidade/Repository/ITermoPrivacidade.repository';
-import { TermoPrivacidadeRepository } from '@modules/Termos-Privacidade/Repository/TermoPrivacidadeRepositort';
 import { UserRepository } from '@modules/User/repository/UserRepository';
 import { IUserRepository } from '@modules/User/repository/UserRepository.interface';
 import { container } from 'tsyringe';
@@ -13,11 +9,6 @@ import { HashProvider } from './providers/hashProvider/implementation/HashProvid
 import { IHashProvider } from './providers/hashProvider/model/IHashProvider';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
-
-container.registerSingleton<IPapelRepository>(
-  'PapelRepository',
-  PapelRepository,
-);
 
 container.registerSingleton<IEstadoRepository>(
   'EstadoRepository',
@@ -29,9 +20,5 @@ container.registerSingleton<ICidadeRepository>(
   CidadeRepository,
 );
 
-container.registerSingleton<ITermoPrivacidadeRepository>(
-  'TermoPrivacidadeRepository',
-  TermoPrivacidadeRepository,
-);
 // utils
 container.registerSingleton<IHashProvider>('HashProvider', HashProvider);
