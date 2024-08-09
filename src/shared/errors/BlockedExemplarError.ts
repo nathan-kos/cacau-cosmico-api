82082ed8558d78420a4f1264cc228d6c0cbce664
nathan ks <nathanks0210@gmail.com>
2024-08-08 21:31:34 -0300

    feat: remove useless code

--- src/shared/errors/BlockedExemplarError.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from './BaseError';
-
-class BlockedExemplarError extends BaseError {
-  constructor(msg: string) {
-    super('Exemplar bloqueado', 409, true, msg);
-  }
-}
-
-export { BlockedExemplarError };
