d1f0db8204e45452d3dbb62573ee25ca5d118aef
nathan ks <nathanks0210@gmail.com>
2024-09-12 22:41:08 -0300

    feat: Add Chocolate entity

---
+++ src/modules/Chocolate/entitie/chocolate.ts
@@ -0,0 +1,21 @@
+class Chocolate {
+  cho_Id: string;
+
+  cho_Nome: string;
+
+  cho_Descricao: string;
+
+  cho_Valor: number;
+
+  cho_Imagem: string;
+
+  cho_Ativo: boolean;
+
+  cho_Peso: number;
+
+  cho_CriadoEm: Date;
+
+  cho_AtualizadoEm: Date;
+}
+
+export { Chocolate };
