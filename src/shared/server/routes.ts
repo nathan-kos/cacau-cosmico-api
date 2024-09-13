61a1d36bcd3b841d788c0e966292694381715533
nathan ks <nathanks0210@gmail.com>
2024-09-12 23:09:40 -0300

    feat: Add Chocolate routes to server routes

--- src/shared/server/routes.ts
+++ src/shared/server/routes.ts
@@ -1,9 +1,12 @@
+import { chocolateRoutes } from '@modules/Chocolate/routes/Chocolate.routes';
 import { userRouter } from '@modules/User/routes/User.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/user', userRouter);
+
+routes.use('/chocolate', chocolateRoutes);
 
 routes.get('/', (request, response) => {
   response.send('Bem Vindo ao API rest da Cacau Cósmico 📚🚀🚀🚀');
