1dc3e938f95f791e9c776b6c4f45cf05611d0af1
nathan ks <nathanks0210@gmail.com>
2024-09-18 23:04:58 -0300

    feat: pedido controller

---
+++ src/modules/Pedido/controller/PedidoController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { AtualizarStatusPedidoService } from '../service/AtualizarStatusPedido.service';
+import { CreatePedidoService } from '../service/CreatePedido.service';
+import { findPedidoByIdService } from '../service/FindPedidoById.service';
+import { ListPedidoByUserIdService } from '../service/ListPedidoByUserId.service';
+
+class PedidoController {
+  async findPedidoById(req: Request, res: Response) {
+    const { ped_Id } = req.params;
+
+    const findPedidoById = container.resolve(findPedidoByIdService);
+
+    const pedido = await findPedidoById.execute(ped_Id);
+
+    return res.status(200).json(pedido);
+  }
+
+  async listPedidoByUserId(req: Request, res: Response) {
+    const { page, limit } = req.query;
+    const { ped_usu_id } = req.params;
+
+    const listPedidoByUserId = container.resolve(ListPedidoByUserIdService);
+
+    const pedidos = await listPedidoByUserId.execute({
+      page: Number(page),
+      limit: Number(limit),
+      filter: {
+        ped_usu_id,
+      },
+    });
+
+    return res.status(200).json(pedidos);
+  }
+
+  async atualizarStatusPedido(req: Request, res: Response) {
+    const { ped_Id } = req.params;
+    const { ped_Status } = req.body;
+
+    const atualizarStatusPedido = container.resolve(
+      AtualizarStatusPedidoService,
+    );
+
+    const pedido = await atualizarStatusPedido.execute(ped_Id, ped_Status);
+
+    return res.status(200).json(pedido);
+  }
+
+  async CreatePedido(req: Request, res: Response) {
+    const { usu_Id, end_Id, frete, cartoes, chocolates, cupons } = req.body;
+
+    const createPedido = container.resolve(CreatePedidoService);
+
+    const pedido = await createPedido.execute({
+      usu_Id,
+      end_Id,
+      frete,
+      cartoes,
+      chocolates,
+      cupons,
+    });
+
+    return res.status(201).json(pedido);
+  }
+}
+
+export { PedidoController };
