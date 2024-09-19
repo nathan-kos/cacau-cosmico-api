46b7036ce298d1709b999f93ae531f155d31c0a9
nathan ks <nathanks0210@gmail.com>
2024-09-18 23:09:54 -0300

    feat: Add Pedido routes to server routes

--- src/shared/server/routes.ts
+++ src/shared/server/routes.ts
@@ -1,4 +1,5 @@
 import { chocolateRoutes } from '@modules/Chocolate/routes/Chocolate.routes';
+import { pedidorouter } from '@modules/Pedido/routes/Pedido.routes';
 import { userRouter } from '@modules/User/routes/User.routes';
 import { Router } from 'express';
 
@@ -7,6 +8,8 @@ const routes = Router();
 routes.use('/user', userRouter);
 
 routes.use('/chocolate', chocolateRoutes);
+
+routes.use('/pedido', pedidorouter);
 
 routes.get('/', (request, response) => {
   response.send('Bem Vindo ao API rest da Cacau Cósmico 📚🚀🚀🚀');
