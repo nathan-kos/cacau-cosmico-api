057bc6ca0515c13dac9f9d3cdad818eb8ec6623c
nathan ks <nathanks0210@gmail.com>
2024-09-18 21:13:04 -0300

    feat: Register new module repositories
    - Register ChocolatePedidoRepository, PedidoRepository, CupomRepository, and CartaoPedidoRepository in the dependency injection container.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,19 @@
 import { CartaoRepository } from '@modules/Cartao/repository/CartaoRepository';
 import { ICartaoRepository } from '@modules/Cartao/repository/ICartaoRepository.interface';
+import { CartaoPedidoRepository } from '@modules/Cartao_Pedido/repository/CartaoPedidoRepository';
+import { ICartaoPedidoRepository } from '@modules/Cartao_Pedido/repository/CartaoPedidoRepository.interface';
 import { Categoria_ChocolateRepository } from '@modules/Categoria_Chocolate/repository/Categoria_ChocolateRepository';
 import { ICategoriaChocolateRepository } from '@modules/Categoria_Chocolate/repository/ICategoria_Chocolate.interface';
 import { ChocolateRepository } from '@modules/Chocolate/repository/ChocolateRepository';
 import { IChocolateRepository } from '@modules/Chocolate/repository/IChocolateRepository.interface';
+import { ChocolatePedidoRepository } from '@modules/Chocolate_Pedido/repository/ChocolatePedidoRepository';
+import { IChocolatePedidoRepository } from '@modules/Chocolate_Pedido/repository/ChocolateRepository.interface';
+import { CupomRepository } from '@modules/Cupom/repository/CupomRepository';
+import { ICupomRepository } from '@modules/Cupom/repository/ICupomRepository.interface';
 import { EnderecoRepository } from '@modules/Endereco/repository/EnderecoRepository';
 import { IEnderecoRepository } from '@modules/Endereco/repository/IEnderecoRepository.interface';
+import { PedidoRepository } from '@modules/Pedido/repository/PedidoRepository';
+import { IPedidoRepository } from '@modules/Pedido/repository/PedidoRepository.interface';
 import { UserRepository } from '@modules/User/repository/UserRepository';
 import { IUserRepository } from '@modules/User/repository/UserRepository.interface';
 import { container } from 'tsyringe';
@@ -32,6 +40,26 @@ container.registerSingleton<IChocolateRepository>(
 container.registerSingleton<ICategoriaChocolateRepository>(
   'CategoriaChocolateRepository',
   Categoria_ChocolateRepository,
+);
+
+container.registerSingleton<IChocolatePedidoRepository>(
+  'ChocolatePedidoRepository',
+  ChocolatePedidoRepository,
+);
+
+container.registerSingleton<IPedidoRepository>(
+  'PedidoRepository',
+  PedidoRepository,
+);
+
+container.registerSingleton<ICupomRepository>(
+  'CupomRepository',
+  CupomRepository,
+);
+
+container.registerSingleton<ICartaoPedidoRepository>(
+  'CartaoPedidoRepository',
+  CartaoPedidoRepository,
 );
 
 // utils
