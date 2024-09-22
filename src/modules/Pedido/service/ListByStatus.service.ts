82eedbcc0e04f0fef6b87d90849eec755f4f3799
nathan ks <nathanks0210@gmail.com>
2024-09-22 18:54:15 -0300

    feat: Add support for retrieving cupons in ListByStatus and ListPedidoByUserId services

--- src/modules/Pedido/service/ListByStatus.service.ts
+++ src/modules/Pedido/service/ListByStatus.service.ts
@@ -2,7 +2,9 @@ import { ICartaoRepository } from '@modules/Cartao/repository/ICartaoRepository.
 import { ICartaoPedidoRepository } from '@modules/Cartao_Pedido/repository/CartaoPedidoRepository.interface';
 import { IChocolateRepository } from '@modules/Chocolate/repository/IChocolateRepository.interface';
 import { IChocolatePedidoRepository } from '@modules/Chocolate_Pedido/repository/ChocolateRepository.interface';
+import { ICupomRepository } from '@modules/Cupom/repository/ICupomRepository.interface';
 import { IEnderecoRepository } from '@modules/Endereco/repository/IEnderecoRepository.interface';
+import { IPedidoCupomRepository } from '@modules/Pedido_Cupom/repository/PedidoCupomRepository.interface';
 import { BadRequestError } from '@shared/errors/BadRequestError';
 import { IPaginatedRequest } from '@shared/interfaces/IPaginatedRequest';
 import { IPaginatedResponse } from '@shared/interfaces/IPaginatedResponse';
@@ -30,6 +32,12 @@ class ListByStatusService {
 
     @inject('EnderecoRepository')
     private enderecoRepository: IEnderecoRepository,
+
+    @inject('PedidoCupomRepository')
+    private pedidoCupomRepository: IPedidoCupomRepository,
+
+    @inject('CupomRepository')
+    private cupomRepository: ICupomRepository,
   ) {}
 
   async execute(
@@ -82,18 +90,39 @@ class ListByStatusService {
           Object.assign(cap, { cartao });
         });
 
+        const pedidoCupom = await this.pedidoCupomRepository.listBy({
+          page: 1,
+          limit: 100,
+          filter: {
+            pcu_ped_id: pedido.ped_Id,
+          },
+        });
+
+        const pedidoCupomPromisse = pedidoCupom.results.map(async (pcu) => {
+          const cupom = await this.cupomRepository.findBy({
+            cup_Id: pcu.pcu_cup_id,
+          });
+
+          Object.assign(pcu, { cupom });
+        });
+
         const endereco = await this.enderecoRepository.findBy({
           end_Id: pedido.ped_end_id,
         });
 
         Object.assign(pedido, { endereco });
 
-        await Promise.all([promisseChocolate, promisseCartao]);
+        await Promise.all([
+          promisseChocolate,
+          promisseCartao,
+          pedidoCupomPromisse,
+        ]);
 
         const cho = chocolatePedido.results;
         const car = cartaoPedido.results;
+        const cup = pedidoCupom.results;
 
-        Object.assign(pedido, { cho, car });
+        Object.assign(pedido, { cho, car, cup });
 
         return pedido;
       }),
