9d527255d4dc9704b9fcb4adaa18962ce91e932d
nathan ks <nathanks0210@gmail.com>
2024-09-30 22:26:07 -0300

    feat: Add TrocaDevolucaoController

---
+++ src/modules/Troca_Devolucao/controller/TrocaDevolucao.controller.ts
@@ -0,0 +1,77 @@
+import { tde_Status } from '@prisma/client';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { AceitarTrocaDevolucaoService } from '../service/AceitarTrocaDevolucao.service';
+import { ListarByChocolatePedidoService } from '../service/ListarByPedido.service';
+import { ListarPorStatusService } from '../service/ListarPorStatus.service';
+import { RejeitarTrocaDevolucaoService } from '../service/RejeitarTrocaDevolucao.service';
+
+class TrocaDevolucaoController {
+  async createTrocaDevolucao(req: Request, res: Response) {
+    const { troca_devolucao } = req.body;
+
+    return res.status(200).json(troca_devolucao);
+  }
+
+  async aceitarTrocaDevolucao(req: Request, res: Response) {
+    const { tde_Id } = req.params;
+
+    const aceitarTrocaDevolucao = container.resolve(
+      AceitarTrocaDevolucaoService,
+    );
+
+    const cupom = await aceitarTrocaDevolucao.execute(tde_Id);
+
+    return res.status(200).json(cupom);
+  }
+
+  async recusarTrocaDevolucao(req: Request, res: Response) {
+    const { tde_Id } = req.params;
+
+    const recusarTrocaDevolucao = container.resolve(
+      RejeitarTrocaDevolucaoService,
+    );
+
+    const trocaDevolucao = await recusarTrocaDevolucao.execute(tde_Id);
+
+    return res.status(200).json(trocaDevolucao);
+  }
+
+  async listTrocaDevolucao(req: Request, res: Response) {
+    const { page, limit } = req.query;
+    const { status } = req.params;
+
+    const listTrocaDevolucao = container.resolve(ListarPorStatusService);
+
+    const trocasDevolucoes = await listTrocaDevolucao.execute({
+      page: Number(page),
+      limit: Number(limit),
+      filter: {
+        tde_Status: status as tde_Status,
+      },
+    });
+
+    return res.status(200).json(trocasDevolucoes);
+  }
+
+  async listTrocaDevolucaoByChocolatePedido(req: Request, res: Response) {
+    const { page, limit } = req.query;
+    const { tde_cho_ped_id } = req.params;
+
+    const listTrocaDevolucao = container.resolve(
+      ListarByChocolatePedidoService,
+    );
+
+    const trocasDevolucoes = await listTrocaDevolucao.execute({
+      page: Number(page),
+      limit: Number(limit),
+      filter: {
+        tde_cho_ped_id,
+      },
+    });
+
+    return res.status(200).json(trocasDevolucoes);
+  }
+}
+
+export { TrocaDevolucaoController };
