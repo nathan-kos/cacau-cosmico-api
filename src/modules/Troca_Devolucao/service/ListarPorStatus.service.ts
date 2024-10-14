4f6bd58603305b83e96d4dd17a9a0b3eb32817dc
nathan ks <nathanks0210@gmail.com>
2024-10-14 15:00:43 -0300

    feat: add objects to troca devolucao listing by status

--- src/modules/Troca_Devolucao/service/ListarPorStatus.service.ts
+++ src/modules/Troca_Devolucao/service/ListarPorStatus.service.ts
@@ -1,4 +1,9 @@
+import { IChocolateRepository } from '@modules/Chocolate/repository/IChocolateRepository.interface';
+import { IChocolatePedidoRepository } from '@modules/Chocolate_Pedido/repository/ChocolateRepository.interface';
+import { IPedidoRepository } from '@modules/Pedido/repository/PedidoRepository.interface';
+import { IUserRepository } from '@modules/User/repository/UserRepository.interface';
 import { BadRequestError } from '@shared/errors/BadRequestError';
+import { EntityNotFoundError } from '@shared/errors/EntityNotFoundError';
 import { IPaginatedRequest } from '@shared/interfaces/IPaginatedRequest';
 import { IPaginatedResponse } from '@shared/interfaces/IPaginatedResponse';
 import { inject, injectable } from 'tsyringe';
@@ -10,6 +15,18 @@ class ListarPorStatusService {
   constructor(
     @inject('TrocaDevolucaoRepository')
     private trocaDevolucaoRepository: ITrocaDevolucaoRepository,
+
+    @inject('ChocolatePedidoRepository')
+    private chocolatePedidoRepository: IChocolatePedidoRepository,
+
+    @inject('ChocolateRepository')
+    private chocolateRepository: IChocolateRepository,
+
+    @inject('PedidoRepository')
+    private pedidoRepository: IPedidoRepository,
+
+    @inject('UserRepository')
+    private userRepository: IUserRepository,
   ) {}
 
   public async execute(
@@ -25,7 +42,44 @@ class ListarPorStatusService {
       limit: data.limit,
     });
 
-    return trocasDevolucoes;
+    const trocasDetalhadas = await Promise.all(
+      trocasDevolucoes.results.map(async (troca) => {
+        // Pega o chocolate pedido relacionado à troca/devolução
+        const chocolatePedido = await this.chocolatePedidoRepository.findBy({
+          chp_Id: troca.tde_cho_ped_id, // Supondo que haja essa relação
+        });
+
+        if (!chocolatePedido) {
+          throw new EntityNotFoundError('Chocolate não encontrado');
+        }
+
+        const chocolate = await this.chocolateRepository.findBy({
+          cho_Id: chocolatePedido.chp_cho_id,
+        });
+        Object.assign(chocolatePedido, { chocolate });
+
+        // Pega o pedido relacionado e, com ele, o usuário
+        const pedido = await this.pedidoRepository.findBy({
+          ped_Id: chocolatePedido.chp_ped_id,
+        });
+
+        const usuario = await this.userRepository.findBy({
+          usu_Id: pedido?.ped_usu_id,
+        });
+
+        Object.assign(troca, { pedido, usuario });
+
+        // const chocolatePedido = chocolatePedido;
+        Object.assign(troca, { chocolatePedido });
+
+        return troca;
+      }),
+    );
+
+    return {
+      ...trocasDevolucoes,
+      results: trocasDetalhadas,
+    };
   }
 }
 
